39d0585e5517438fa14a1cd387707224123d731b
Ryan Benasutti <firey45@gmail.com>
2020-01-29 14:39:27 -0500

    Remove outdated stuff from the script

--- seaLinkGen.groovy
+++ seaLinkGen.groovy
@@ -110,26 +110,26 @@ class MyCadGen implements ICadGenerator {
         println("Computed centerOfMassFromCentroid=" + centerOfMassFromCentroid)
 
 
-        tmpSrv.setManipulator(manipulator)
-        allCad.add(tmpSrv)
-        println "Generating link: "+linkIndex
+//        tmpSrv.setManipulator(manipulator)
+//        allCad.add(tmpSrv)
+//        println "Generating link: "+linkIndex
 
-        if (i == 0) {
-            // more at https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/DHLink.java
-            println dh
-            println "D = "+dh.getD()// this is the height of the link
-            println "R = "+dh.getR()// this is the radius of rotation of the link
-            println "Alpha = "+Math.toDegrees(dh.getAlpha())// this is the alpha rotation
-            println "Theta = "+Math.toDegrees(dh.getTheta())// this is the rotation about hte final like orentation
-            println conf // gets the link hardware map from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/LinkConfiguration.java
-            println conf.getHardwareIndex() // gets the link hardware index
-            println conf.getScale() // gets the link hardware scale to degrees from link units
-            // more from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/AbstractLink.java
-            println  "Max engineering units for link = " + abstractLink.getMaxEngineeringUnits()
-            println  "Min engineering units for link = " + abstractLink.getMinEngineeringUnits()
-            println "Position "+abstractLink.getCurrentEngineeringUnits()
-            println manipulator
-        }
+//        if (i == 0) {
+//            // more at https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/DHLink.java
+//            println dh
+//            println "D = "+dh.getD()// this is the height of the link
+//            println "R = "+dh.getR()// this is the radius of rotation of the link
+//            println "Alpha = "+Math.toDegrees(dh.getAlpha())// this is the alpha rotation
+//            println "Theta = "+Math.toDegrees(dh.getTheta())// this is the rotation about hte final like orentation
+//            println conf // gets the link hardware map from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/LinkConfiguration.java
+//            println conf.getHardwareIndex() // gets the link hardware index
+//            println conf.getScale() // gets the link hardware scale to degrees from link units
+//            // more from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/AbstractLink.java
+//            println  "Max engineering units for link = " + abstractLink.getMaxEngineeringUnits()
+//            println  "Min engineering units for link = " + abstractLink.getMinEngineeringUnits()
+//            println "Position "+abstractLink.getCurrentEngineeringUnits()
+//            println manipulator
+//        }
 
         return allCad;
     }
