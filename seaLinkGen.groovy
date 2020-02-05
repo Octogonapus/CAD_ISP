174704e5cf662624f4d8053bf1cb6a34ad1ee40f
Ryan Benasutti <firey45@gmail.com>
2020-02-05 14:29:24 -0500

    Cleanup

--- seaLinkGen.groovy
+++ seaLinkGen.groovy
@@ -1,16 +1,20 @@
-import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
+import com.neuronrobotics.bowlerstudio.creature.ICadGenerator
 import com.neuronrobotics.bowlerstudio.creature.CreatureLab
 import com.neuronrobotics.bowlerstudio.physics.TransformFactory
+import com.neuronrobotics.sdk.addons.kinematics.AbstractLink
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR
-import eu.mihosoft.vrl.v3d.CSG;
-import org.apache.commons.io.IOUtils;
-import com.neuronrobotics.bowlerstudio.vitamins.*;
-import java.nio.file.Paths;
-import eu.mihosoft.vrl.v3d.FileUtil;
-import com.neuronrobotics.bowlerstudio.vitamins.*;
-import javafx.scene.transform.Affine;
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Transform
+import javafx.scene.paint.Color
+import org.apache.commons.io.IOUtils
+import com.neuronrobotics.bowlerstudio.vitamins.*
+import java.nio.file.Paths
+import eu.mihosoft.vrl.v3d.FileUtil
+import com.neuronrobotics.bowlerstudio.vitamins.*
+import javafx.scene.transform.Affine
 import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration
 import com.neuronrobotics.sdk.addons.kinematics.DHLink
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
@@ -18,13 +22,13 @@ import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 CSG reverseDHValues(CSG incoming, DHLink dh) {
     println "Reversing " + dh
     TransformNR step = new TransformNR(dh.DhStep(0))
-    Transform move = com.neuronrobotics.bowlerstudio.physics.TransformFactory.nrToCSG(step)
+    Transform move = TransformFactory.nrToCSG(step)
     return incoming.transformed(move)
 }
 
 CSG moveDHValues(CSG incoming, DHLink dh) {
     TransformNR step = new TransformNR(dh.DhStep(0)).inverse()
-    Transform move = com.neuronrobotics.bowlerstudio.physics.TransformFactory.nrToCSG(step)
+    Transform move = TransformFactory.nrToCSG(step)
     return incoming.transformed(move)
 
 }
@@ -34,26 +38,26 @@ class MyCadGen implements ICadGenerator {
     @Override
     ArrayList<CSG> generateBody(MobileBase mobileBase) {
         CSG body = new Cube(30).toCSG()
-        body.setColor(javafx.scene.paint.Color.WHITE)
-        body.setManipulator(mobileBase.getRootListener());
-        return [body];
+        body.setColor(Color.WHITE)
+        body.setManipulator(mobileBase.getRootListener())
+        return [body]
     }
 
     @Override
     ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
         println("Generating CAD for " + d + " linkIndex=" + linkIndex)
-        def vitaminLocations = new HashMap<TransformNR, ArrayList<String>>()
 
-        ArrayList<DHLink> dhLinks = d.getChain().getLinks()
-        ArrayList<CSG> allCad = new ArrayList<CSG>()
-        int i = linkIndex;
+        Map<TransformNR, List<String>> vitaminLocations = new HashMap<TransformNR, List<String>>()
+        List<DHLink> dhLinks = d.getChain().getLinks()
+        List<CSG> allCad = new ArrayList<CSG>()
+
         DHLink dh = dhLinks.get(linkIndex)
         // Hardware to engineering units configuration
-        LinkConfiguration conf = d.getLinkConfiguration(i);
+        LinkConfiguration conf = d.getLinkConfiguration(linkIndex)
         // Engineering units to kinematics link (limits and hardware type abstraction)
-        AbstractLink abstractLink = d.getAbstractLink(i) as AbstractLink;
+        AbstractLink abstractLink = d.getAbstractLink(linkIndex)
         // Transform used by the UI to render the location of the object
-        Affine manipulator = dh.getListener();
+        Affine manipulator = dh.getListener()
 
         TransformNR locationOfMotorMount = new TransformNR(dh.DhStep(0)).inverse()
         vitaminLocations.put(
@@ -64,10 +68,10 @@ class MyCadGen implements ICadGenerator {
         if (linkIndex != d.getNumberOfLinks() - 1) {
             // If this is not the last link (the last link does not get a motor)
             // The motor for the first link is part of the base
-            LinkConfiguration confPrior = d.getLinkConfiguration(i + 1)
+            LinkConfiguration confPrior = d.getLinkConfiguration(linkIndex + 1)
             vitaminLocations.put(
                     new TransformNR(),
-                    [confPrior.getElectroMechanicalType(), confPrior.getElectroMechanicalSize()] as ArrayList<String>
+                    [confPrior.getElectroMechanicalType(), confPrior.getElectroMechanicalSize()]
             )
         }
 
@@ -80,7 +84,9 @@ class MyCadGen implements ICadGenerator {
             def vitaminSize = vitaminLocations.get(vitaminLocation)[1]
             def vitaminData = Vitamins.getConfiguration(vitaminType, vitaminSize)
             def vitaminCad = Vitamins.get(vitaminType, vitaminSize)
-            println(vitaminData)
+
+            println("Adding vitamin to CoM: " + vitaminData)
+
             def massKg = vitaminData["massKg"] as double
             def comCentroid = vitaminLocation.times(
                     new TransformNR(
@@ -111,29 +117,7 @@ class MyCadGen implements ICadGenerator {
         println("Computed totalMassKg=" + totalMassKg)
         println("Computed centerOfMassFromCentroid=" + centerOfMassFromCentroid)
 
-
-//        tmpSrv.setManipulator(manipulator)
-//        allCad.add(tmpSrv)
-//        println "Generating link: "+linkIndex
-
-//        if (i == 0) {
-//            // more at https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/DHLink.java
-//            println dh
-//            println "D = "+dh.getD()// this is the height of the link
-//            println "R = "+dh.getR()// this is the radius of rotation of the link
-//            println "Alpha = "+Math.toDegrees(dh.getAlpha())// this is the alpha rotation
-//            println "Theta = "+Math.toDegrees(dh.getTheta())// this is the rotation about hte final like orentation
-//            println conf // gets the link hardware map from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/LinkConfiguration.java
-//            println conf.getHardwareIndex() // gets the link hardware index
-//            println conf.getScale() // gets the link hardware scale to degrees from link units
-//            // more from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/AbstractLink.java
-//            println  "Max engineering units for link = " + abstractLink.getMaxEngineeringUnits()
-//            println  "Min engineering units for link = " + abstractLink.getMinEngineeringUnits()
-//            println "Position "+abstractLink.getCurrentEngineeringUnits()
-//            println manipulator
-//        }
-
-        return allCad;
+        return allCad
     }
 }
 
